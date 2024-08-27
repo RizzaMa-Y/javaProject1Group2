d7c4a2e520fdd9642efed7df6ebe0ad45110f43d
RizzaMa-Y <rizza.may.magsakay@pwc.com>
2024-08-27 16:30:29 +0800

    aug27 update

--- javaproject4/src/module-info.java
+++ javaproject4/src/module-info.java
@@ -8,6 +8,8 @@
 module javaproject4 {
 	requires java.desktop;
 	requires java.sql;
+	//requires jdatepicker;
+	//requires itextpdf;
 //	requires rs2xml;
 //	requires com.itextpdf.kernel;
 //  requires com.itextpdf.layout;
